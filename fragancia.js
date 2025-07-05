781e013a51d053dc45c9432cf4f143c7af66ad8d
Cecilia Denis <ceciliadendil@gmail.com>
2025-07-05 12:23:31 -0300

    version 2

--- fragancia.js
+++ fragancia.js
@@ -1,28 +1,34 @@
-const Perfumes = [
-    {tipo:"Floral",nombre:"Jazmin Suave",precio:120},
-    {tipo:"Amaderado",nombre:"Cedro del bosque", precio:150},
-    {tipo:"Frutal",nombre:"Pasión",precio:110}
+const perfumes = [
+    { tipo: "Floral", nombre: "Jazmin Suave", precio: 120 },
+    { tipo: "Amaderado", nombre: "Cedro del bosque", precio: 150 },
+    { tipo: "Frutal", nombre: "Pasión", precio: 110 }
 ];
 
-console.log(Perfumes[2])
 
-let perfume = prompt (`Qué tipo de perfume estás buscando?
+function buscarPerfume() {
+    const perfume = prompt(`¿Qué tipo de perfume estás buscando?
     1 - Floral
     2 - Amaderado
-    3 - Frutal
-    `);
+    3 - Frutal`);
 
-switch (perfume) {
-  case "1":
-    alert("Nombre: Jazmin Suave, precio:$120");
-    break;
-  case "2":
-    alert("Nombre: Cedro del bosque, precio:$150");
-    break;
-  case "3":
-    alert("Nombre: Pasión, precio:$110");
-    break;
-  default:
-    alert("La opción que ingresaste no es válida");
-    break;
+    switch (perfume) {
+        case "1":
+            alert("Elegiste el perfume 1: Jazmin Suave - precio: $120");
+            console.log(perfumes[0]);
+            break;
+        case "2":
+            alert("Elegiste el perfume 2: Cedro del bosque - precio: $150");
+            console.log(perfumes[1]);
+            break;
+        case "3":
+            alert("Elegiste el perfume 3: Pasión - precio: $110");
+            console.log(perfumes[2]);
+            break;
+        default:
+            alert("La opción que ingresaste no es válida");
+            console.log("Entrada inválida")
+            break;
+    }
 }
+
+buscarPerfume();
