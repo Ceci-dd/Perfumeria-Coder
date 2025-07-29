b927189493ab7b0abfa3a463ded8f73940ec44c5
Cecilia Denis <ceciliadendil@gmail.com>
2025-07-28 21:47:30 -0300

    segunda etrega agregar DOM y eventos

--- fragancia.js
+++ fragancia.js
@@ -1,34 +1,111 @@
+const shopcontent = document.getElementById("shopcontent");
+const carritocontent = document.getElementById("carrito");
+const inputBuscar = document.getElementById("inputprincipal");
+const botonBuscar = document.querySelector(".inputbuscar button");
+
+
+
 const perfumes = [
-    { tipo: "Floral", nombre: "Jazmin Suave", precio: 120 },
-    { tipo: "Amaderado", nombre: "Cedro del bosque", precio: 150 },
-    { tipo: "Frutal", nombre: "Pasión", precio: 110 }
+  { tipo: "Floral", nombre: "Jazmin Suave", precio: 120, img: "medios/perfumejazminsuave.jpeg"},
+  { tipo: "Amaderado", nombre: "Cedro del bosque", precio: 150, img: "medios/perfumecedrodelbosque.jpeg"},
+  { tipo: "Frutal", nombre: "Pasión", precio: 110, img: "medios/perfumepasion.jpeg" }
 ];
 
+let carrito = [];
 
-function buscarPerfume() {
-    const perfume = prompt(`¿Qué tipo de perfume estás buscando?
-    1 - Floral
-    2 - Amaderado
-    3 - Frutal`);
+perfumes.forEach((perfume) => {
+  const card = document.createElement("div");
+  card.className = "cardstyle";
+  card.innerHTML = `
+    <img src="${perfume.img}" alt="" class="perfumeimg"/>
+    <h3>${perfume.nombre}</h3>
+    <p>Tipo: ${perfume.tipo}</p>
+    <p class="precio">Precio: $${perfume.precio}</p>
+  `;
 
-    switch (perfume) {
-        case "1":
-            alert("Elegiste el perfume 1: Jazmin Suave - precio: $120");
-            console.log(perfumes[0]);
-            break;
-        case "2":
-            alert("Elegiste el perfume 2: Cedro del bosque - precio: $150");
-            console.log(perfumes[1]);
-            break;
-        case "3":
-            alert("Elegiste el perfume 3: Pasión - precio: $110");
-            console.log(perfumes[2]);
-            break;
-        default:
-            alert("La opción que ingresaste no es válida");
-            console.log("Entrada inválida")
-            break;
+  const AgregarAlCarrito = document.createElement("button");
+  AgregarAlCarrito.innerText = "Agregar al Carrito";
+  AgregarAlCarrito.className = "botoncarrito";
+
+  AgregarAlCarrito.addEventListener("click", () => {
+    carrito.push({
+      nombre: perfume.nombre,
+      tipo: perfume.tipo,
+      precio: perfume.precio
+    });
+
+    mostrarCarrito();
+  });
+
+  card.appendChild(AgregarAlCarrito);
+  shopcontent.appendChild(card);
+});
+
+botonBuscar.addEventListener("click", () => {
+  const texto = inputBuscar.value.trim().toLowerCase();
+  shopcontent.innerHTML = "";
+
+  perfumes.forEach(perfume => {
+    if (perfume.nombre.toLowerCase().includes(texto) || texto === "") {
+      const card = document.createElement("div");
+      card.className = "cardstylesolo";
+      card.innerHTML = `
+        <img src="${perfume.img}" alt="" class="perfumeimgsola"/>
+        <h3>${perfume.nombre}</h3>
+        <p>Tipo: ${perfume.tipo}</p>
+        <p class="precio">Precio: $${perfume.precio}</p>
+      `;
+
+      const botonAgregar = document.createElement("button");
+      botonAgregar.innerText = "Agregar al Carrito";
+      botonAgregar.className = "botoncarrito";
+
+      botonAgregar.addEventListener("click", () => {
+        carrito.push({
+          nombre: perfume.nombre,
+          tipo: perfume.tipo,
+          precio: perfume.precio
+        });
+        mostrarCarrito();
+      });
+
+      card.appendChild(botonAgregar);
+      shopcontent.appendChild(card);
     }
+  });
+});
+
+function mostrarCarrito() {
+  carritocontent.innerHTML = ""; 
+
+  carrito.forEach((item, index) => {
+    const itemDiv = document.createElement("div");
+    itemDiv.className = "item-carrito";
+    itemDiv.innerHTML = `
+      <p class="itemscarrito">${item.nombre} - ${item.tipo} - $${item.precio}</p>
+      <button onclick="eliminarDelCarrito(${index})" class="eliminarbutton">Eliminar</button>
+    `;
+
+
+    carritocontent.appendChild(itemDiv);
+
+  });
 }
 
-buscarPerfume();
+
+
+
+
+function eliminarDelCarrito(index) {
+  carrito.splice(index, 1); 
+  mostrarCarrito(); 
+}
+
+
+
+
+
+
+
+
+
